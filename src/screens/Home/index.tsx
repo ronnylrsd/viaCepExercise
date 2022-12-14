19a8ff78b844d0f49423701c38835942aa1e34ca
Ronny Lima Ribeiro da Silva <63603061+ronnylrsd@users.noreply.github.com>
2022-12-14 09:41:24 -0300

    Feat: trying to create function

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,6 +3,10 @@ import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
 
 export function Home(){
+    function handleCepSearch(cep: string){
+        console.log(cep);
+    }
+
     return (
         <View style={styles.container}>
             <TextInput
@@ -11,7 +15,7 @@ export function Home(){
                 placeholderTextColor="#6B6B6B"
             />
 
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button} onPress={handleCepSearch}>
             <Text style={styles.buttonText}>Consultar CEP</Text>
         </TouchableOpacity>
         </View>
