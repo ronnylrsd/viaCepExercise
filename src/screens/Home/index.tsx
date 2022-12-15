fdaf215c3c8d2a2653cd4a235a0e30535768d93a
Ronny Lima Ribeiro da Silva <63603061+ronnylrsd@users.noreply.github.com>
2022-12-15 11:11:57 -0300

    Feat: using clean code

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,9 +4,27 @@ import { View, Text, TextInput, TouchableOpacity } from "react-native";
 
 import { styles } from "./styles";
 
+import api from "../../services/api";
+
 export function Home() {
-  const [cepInfo, setCepInfo] = useState({});
+  const [cepInfo, setCepInfo] = useState({
+    cep: "",
+    logradouro: "",
+    complemento: "",
+    bairro: "",
+    localidade: "",
+    uf: "",
+  });
   const [cepSearch, setCepSearch] = useState("");
+
+  const getCep = async () => {
+    try {
+      const response = await api.get(`${cepSearch}/json/`);
+      setCepInfo(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   function handleCepSearch(cep: string) {
     console.log(cep);
@@ -21,7 +39,7 @@ export function Home() {
         value={cepSearch}
         onChangeText={(cep) => setCepSearch(cep)}
       />
-      <TouchableOpacity style={styles.button}>
+      <TouchableOpacity style={styles.button} onPress={getCep}>
         <Text style={styles.buttonText}>Consultar CEP</Text>
       </TouchableOpacity>
 
@@ -29,6 +47,7 @@ export function Home() {
         <Text style={styles.cepCardTitle}>As informações do CEP são:</Text>
         <Text style={styles.cepCardText}>CEP: {cepInfo.cep}</Text>
         <Text style={styles.cepCardText}>Logradouro: {cepInfo.logradouro}</Text>
+        <Text style={styles.cepCardText}>Complemento: {cepInfo.complemento}</Text>
         <Text style={styles.cepCardText}>Bairro: {cepInfo.bairro}</Text>
         <Text style={styles.cepCardText}>Cidade: {cepInfo.localidade}</Text>
         <Text style={styles.cepCardText}>Estado: {cepInfo.uf}</Text>
